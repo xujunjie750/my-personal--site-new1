b224889a06e63b531987bae89361574da3b24d4f
xujunjie750 <838549236@qq.com>
2026-02-09 13:03:04 +0800

    fix: add missing components

---
+++ src/components/ChatWindow.jsx
@@ -0,0 +1,13 @@
+// This file is created to resolve the build error.
+// Please add the necessary component logic here.
+import React from 'react';
+
+const ChatWindow = () => {
+  return (
+    <div>
+      {/* Chat window content goes here */}
+    </div>
+  );
+};
+
+export default ChatWindow;
